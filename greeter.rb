e3780332240561571e53d98e759744ab79635985
highcastle1945 <highcastle7@yahoo.com>
2015-05-06 21:34:17 -0400

    Coding 101 and tutorial ruby files
    Ruby files as part of coding 101 ep. 1 and web tutorial

---
+++ greeter.rb
@@ -0,0 +1,15 @@
+class Greeter
+    def initialize(name = "World")
+        @name = name
+    end
+    def say_hi
+        puts "Hi #{@name}!"
+    end
+    def say_bye
+        puts "Bye #{@name}, come back soon."
+    end
+end
+
+g=Greeter.new("Pat")
+g.say_hi
+g.say_bye
